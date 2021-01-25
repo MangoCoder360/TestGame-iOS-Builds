2c19afb7543d119a6b2bb34cb93a39168d9cd0dc
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2021-01-25 14:10:55 -0800

    Imported project files

---
+++ www/code0.js
@@ -0,0 +1,20 @@
+gdjs.Scene1Code = {};
+
+gdjs.Scene1Code.conditionTrue_0 = {val:false};
+gdjs.Scene1Code.condition0IsTrue_0 = {val:false};
+
+
+gdjs.Scene1Code.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Scene1Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Scene1Code.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['Scene1Code'] = gdjs.Scene1Code;
